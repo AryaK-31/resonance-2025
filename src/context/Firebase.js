5fbad84c3ce90dfdbca98f046435efb824a5dd1f
Arya Kaslikar <aryakaslikar31@gmail.com>
2025-07-18 20:08:45 +0530

    api

--- src/context/Firebase.js
+++ src/context/Firebase.js
@@ -3,12 +3,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    apiKey: "AIzaSyCh8G3b1rPqzyKr31D85yRpOpy1YWwZWz0",
+    authDomain: "resonance-70ed1.firebaseapp.com",
+    projectId: "resonance-70ed1",
+    storageBucket: "resonance-70ed1.firebasestorage.app",
+    messagingSenderId: "652126282202",
+    appId: "1:652126282202:web:a5fcb706281a523fb5e0e3"
 };
 
 // Initialize Firebase
